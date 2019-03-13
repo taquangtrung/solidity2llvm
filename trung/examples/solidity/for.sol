b09596e8845ce7b0b7157e27a0aa1bfb88d8d1ba
trungtq <taquangtrungvn@gmail.com>
2019-03-13 12:37:47 +0800

    restructure

---
+++ trung/examples/solidity/for.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+        int n = 10;
+        int a = 1;
+        for (int i = 0; i < n; i++)
+            a = a + i;
+        a = a + 2;
+    }
+}
